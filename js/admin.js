7d781e08eab2012a7c9a94a8135b9f2ea356a195
walidkarouach <karouachwalid700@gmail.com>
2026-05-22 21:23:59 +0100

    Ajouter DOM de la page admin

---
+++ js/admin.js
@@ -0,0 +1,35 @@
+const tableBody =
+document.getElementById("tableBody");
+
+const adminModal =
+document.getElementById("adminModal");
+
+const openAddModal =
+document.getElementById("openAddModal");
+
+const cancelBtn =
+document.getElementById("cancelBtn");
+
+const bookForm =
+document.getElementById("bookForm");
+
+/* inputs */
+
+const bookId =
+document.getElementById("bookId");
+
+const titleInput =
+document.getElementById("title");
+
+const authorInput =
+document.getElementById("author");
+
+const genreInput =
+document.getElementById("genre");
+
+const descriptionInput =
+document.getElementById("description");
+
+const coverInput =
+document.getElementById("cover");
+
